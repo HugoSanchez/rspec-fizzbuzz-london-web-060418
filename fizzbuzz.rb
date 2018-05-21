a7229d1fbea7a650cf11407b7d59a4f62346fe5e
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-21 10:52:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    return "Fizz"
+  if int % 3 == 0 && int % 5 == 0   
+    return "Fizzbuzz"
   elsif int % 5 == 0 
     return "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0   
-    return "Fizzbuzz"
+  elsif int % 3 == 0   
+    return "Fizz"
 #    return "Fizz"
 #  elsif num/5 == true
 #    return "Buzz"
