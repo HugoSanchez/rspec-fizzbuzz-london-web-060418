41f1a117d4b800c36f2571873517618f9c5ce237
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-21 10:31:56 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(num)
+  if num/3 == true 
+    return "Fizz"
+  elsif num/5 == true
+    return "Buzz"
+  elsif num/3 & & num/5 == true
+    return "FizzBuzz"
+  else
+    return nil 
+  end
+end
